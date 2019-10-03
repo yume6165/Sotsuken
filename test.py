196c81cb4e009c28a300e50acf7e42e0cb32237c
Yume Asayama <dyruemeem1332@keio.jp>
2019-10-03 18:03:08 +0900

    Test OpenCV

--- test.py
+++ test.py
@@ -1,7 +1,11 @@
+#codeing:utf-8
+# -*- coding: utf-8 -*-
+
 import os, sys, time
 import cv2 as cv
-from PIL import Image, ImageTk
+from PIL import Image
 
 if __name__ == '__main__':
-        image = cv.imread(".\sample\incision_1.jpg")
-        cv.imshow(image)
+        img = cv.imread("./sample/incision_1.jpg")
+        cv.imshow("ウィンドウ",img)
+        cv.waitKey()
