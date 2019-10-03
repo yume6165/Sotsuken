233a48688e0aa52a8aa9f0eeaa18bd773c7eef0b
Yume Asayama <dyruemeem1332@keio.jp>
2019-10-03 18:45:49 +0900

    GrayScale

--- test.py
+++ test.py
@@ -4,8 +4,17 @@
 import os, sys, time
 import cv2 as cv
 from PIL import Image
+import numpy as np
+
+def find_rect():#グラフカットのための長方形を決定するための関数
+    global tmp_img#画像処理のための一時的な保管場所
+    tmp_img = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+
+    return 0
+
 
 if __name__ == '__main__':
         img = cv.imread("./sample/incision_1.jpg")
-        cv.imshow("ウィンドウ",img)
+        find_rect()
+        cv.imshow("ウィンドウ",tmp_img)
         cv.waitKey()
